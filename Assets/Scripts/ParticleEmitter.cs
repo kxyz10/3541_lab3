55112acf7abd95c1a0a560352826226add616215
Kevin Oliver <oliver.395@osu.edu>
2021-02-13 22:30:26 -0500

    Adding particle emitter

---
+++ Assets/Scripts/ParticleEmitter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEmitter : MonoBehaviour
+{
+
+    private GameObject particleEmitter;
+    public int particlesPerTime = 1;
+    Particle particle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particleEmitter = new GameObject("Particle Emitter");
+        particleEmitter.transform.position = new Vector3(0, 10, 0);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Particle newParticle = Instantiate(particle, new Vector3(Random.Range(0, 10), Random.Range(0, 10), Random.Range(0, 10)), transform.rotation);
+        particle.transform.position = new Vector3(Random.Range(0, 10), Random.Range(0, 10), Random.Range(0, 10));
+    }
+}
